cc2eb6e9a8ba54f28258ed31b2297d2ab41884ea
Habib R <Habib.R@outlook.com>
2015-07-09 14:02:58 +0100

    add plot method

--- src/BD_LERW.py
+++ src/BD_LERW.py
@@ -19,6 +19,7 @@ x = 0   # x coordinate of starting location
 # y coordinate of starting location. Origin is at centre of square
 y = Circ / 2
 s = 0  # Step number.
+realizations = 8
 
 trajectory = []   # List of the x coordinates of all points visited.
 # (Length x Circ) 2D array of zeros
@@ -34,75 +35,92 @@ ax.set_ylim(0, Circ - 1)
 ax.get_xaxis().set_visible(False)
 ax.get_yaxis().set_visible(False)
 
+def plot(LERW, c='g', Length = Length, Circ = Circ):
+	for pos in range(len(LERW)):
+		x, y = LERW[pos]
+		if (x == Length) or (x == 0) or (y == Circ) or (y == Circ) or (y == 0):
+			LERW[pos] = (np.nan, np.nan)
+		pos += 1
+	plt.plot(*zip(*LERW), color=c, linewidth=0.2)
 
 # Generate a randomwalk
-while True:
-	s += 1
-	if (bool(random.getrandbits(1))):
+for i in range(realizations):
+	s = 0
+	x = 0 # x coordinate of starting location
+	y = Circ / 2 # y coordinate of starting location
+	lattice = np.zeros((Length, Circ), dtype=int)
+	trajectory = []
+
+	while True:
+		s += 1
 		if (bool(random.getrandbits(1))):
-			x += 1
+			if (bool(random.getrandbits(1))):
+				x += 1
+			else:
+				x -= 1
 		else:
-			x -= 1
-	else:
-		if (bool(random.getrandbits(1))):
-			y += 1
-		else:
-			y -= 1
+			if (bool(random.getrandbits(1))):
+				y += 1
+			else:
+				y -= 1
 
-	if (x >= Length):
-		break
-	elif (x < 0):
-		x = 0
-	if (y >= Circ):
-		y -= Circ
-	elif (y < 0):
-		y += Circ
+		if (x >= Length):
+			break
+		elif (x < 0):
+			x = 0
+		if (y >= Circ):
+			y -= Circ
+		elif (y < 0):
+			y += Circ
 
-	lattice[x][y] += 1
-	trajectory.append((x, y))
+		lattice[x][y] += 1
+		trajectory.append((x, y))
 
-plt.plot(*zip(*trajectory), color='r', linewidth=0.3)
-
-# Loop erasure (tranversal from left to right)
-lerw = deepcopy(trajectory)
-lcpy = deepcopy(lattice)
-x0, y0 = None, None
-pos = 0
-
-while pos < len(lerw):
-	x, y = lerw[pos]
-	if lcpy[x][y] > 1 and (not x0):
-		x0, y0 = x, y
-		pos0 = pos
-	elif (x == x0) and (y == y0) and (lcpy[x][y] == 1):
-		del lerw[pos0:pos]
-		x0, y0 = None, None
-		pos = pos0
-	lcpy[x][y] -= 1
-	pos += 1
-
-plt.plot(*zip(*lerw), color='b', linewidth=0.3)
+	x0, y0, pos = None, None, 0
 
 
-# Loop erasure (tranversal from right to left)
-lerw = deepcopy(trajectory[::-1])
-lcpy = deepcopy(lattice)
-x0, y0 = None, None
-pos = 0
+	# Loop erasure
+	LERW_LeftRight = deepcopy(trajectory)
+	lcpy = deepcopy(lattice)
+	x0, y0 = None, None
+	pos = 0
 
-while pos < len(lerw):
-	x, y = lerw[pos]
-	if lcpy[x][y] > 1 and (not x0):
-		x0, y0 = x, y
-		pos0 = pos
-	elif (x == x0) and (y == y0) and (lcpy[x][y] == 1):
-		del lerw[pos0:pos]
-		x0, y0 = None, None
-		pos = pos0
-	lcpy[x][y] -= 1
-	pos += 1
+	while pos < len(LERW_LeftRight):
+		x, y = LERW_LeftRight[pos]
+		if lcpy[x][y] > 1 and (not x0):
+			x0, y0 = x, y
+			pos0 = pos
+		elif (x == x0) and (y == y0) and (lcpy[x][y] == 1):
+			del LERW_LeftRight[pos0:pos]
+			x0, y0 = None, None
+			pos = pos0
+		lcpy[x][y] -= 1
+		pos += 1
 
-plt.plot(*zip(*lerw), color='g', linewidth=0.3)
+
+	plot(LERW_LeftRight)
+
+	# Loop erasure (tranversal from right to left)
+	LERW_RightLeft = deepcopy(trajectory[::-1])
+	lcpy = deepcopy(lattice)
+	x0, y0 = None, None
+	pos = 0
+
+	while pos < len(LERW_RightLeft):
+		x, y = LERW_RightLeft[pos]
+		if lcpy[x][y] > 1 and (not x0):
+			x0, y0 = x, y
+			pos0 = pos
+		elif (x == x0) and (y == y0) and (lcpy[x][y] == 1):
+			del LERW_RightLeft[pos0:pos]
+			x0, y0 = None, None
+			pos = pos0
+		lcpy[x][y] -= 1
+		pos += 1
+
+	plot(LERW_RightLeft, 'r')
+
+
 
 # Plot random walk
-plt.savefig("plots/"+__file__[:-3]+".png", bbox_inches="tight", dpi=dpi)
+plt.savefig(__file__[:-3]+".png", bbox_inches="tight", dpi=dpi)
