3dc5a71c97d5392dc59a98ba32ac2282d16dbc74
Habib R <Habib.R@outlook.com>
2016-02-21 15:10:05 +0000

    update graphs

--- src/LoopErasedRandomWalk.py
+++ src/LoopErasedRandomWalk.py
@@ -17,7 +17,7 @@ Circ = 200  # circumference of cyclinder
 x = 0   # x coordinate of starting location
 # y coordinate of starting location. Origin is at centre of square
 y = Circ / 2
-s = 0  # Step number.
+s = 7  # Step number.
 
 trajectory = []   # List of the x coordinates of all points visited.
 # (Length x Circ) 2D array of zeros
@@ -78,4 +78,4 @@ ax.set_ylim(0, Circ - 1)
 ax.get_xaxis().set_visible(False)
 ax.get_yaxis().set_visible(False)
 plt.plot(*zip(*trajectory), linewidth=0.3)
-plt.savefig("plots/"+__file__[:-3]+".png", bbox_inches="tight", dpi=dpi)
+plt.savefig(__file__[:-3]+".png", bbox_inches="tight", dpi=dpi)
